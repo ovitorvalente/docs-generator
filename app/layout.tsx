15a055eb5e427c948cfa9039d9f5ad678f230845
ovitorvalente <ovitorvalente@gmail.com>
2026-04-14 14:15:04 -0300

    refactor(layout): refatorar layout
    - atualiza layout em app

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { Toaster } from "@/components/ui/sonner";
+import { ThemeProvider } from "@/components/system/theme-provider";
 
 const inter = Inter({
   subsets: ["latin"],
@@ -19,12 +20,12 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="pt-BR" suppressHydrationWarning className="dark">
-      <body
-        className={`${inter.variable} antialiased`}
-      >
-        {children}
-        <Toaster />
+    <html lang="pt-BR" suppressHydrationWarning>
+      <body className={`${inter.variable} antialiased`}>
+        <ThemeProvider>
+          {children}
+          <Toaster />
+        </ThemeProvider>
       </body>
     </html>
   );
